db303f6b12add88233c00c83e6a4ede7e95d462f
Mariws <marius.enachem@gmail.com>
2021-07-24 16:20:44 +0300

    improve script words

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log('hELLO wORLD !!')
 
  
-let verbs1Array = ['should ', 'need ', "shouldn't ", 'must not ', 'will improve if you ', 'be aware when you '];
+let verbs1Array = ['should ', 'need to ', "shouldn't ", 'must not ', 'will improve if you ', 'be aware when you ', 'will understand when you ', 'will find the answer when you ', 'will be in trouble if you '];
 
-let verbs2Array = ['eat ', 'walk ', 'feed ', 'cut ', 'exercise ', 'trust ', 'drive '];
+let verbs2Array = ['eat ', 'walk ', 'feed ', 'cut ', 'exercise ', 'trust ', 'drive ', 'tame ', 'pass ', 'release ', 'pet ', 'burn ', 'play ', 'respect ', 'praise '];
 
-let nounsArray = ['the cheese ', 'the dog ', 'the trees ', 'the fats ', 'mode ', 'less ', 'the bear ', 'no one ', 'all ', 'dumbbells '];
+let nounsArray = ['the cheese ', 'the dog ', 'the trees ', 'the fats ', 'more ', 'less ', 'the bear ', 'no one ', 'everyone ', 'dumbbells ', 'the rats ', 'the cow ', 'the bacon '];
 
 
 
@@ -14,5 +14,5 @@ function getRandWord(fromThisArray){
     return fromThisArray[x];
 }
 
-console.log("You " + getRandWord(verbs1Array) + getRandWord(verbs2Array) + getRandWord(nounsArray) + "!");
+console.log("You " + getRandWord(verbs1Array) + getRandWord(verbs2Array) + getRandWord(nounsArray) + "!!");
 
